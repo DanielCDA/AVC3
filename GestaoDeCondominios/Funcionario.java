b5efbce8810fb4eec8b53ab0fcac72c128009248
AntonioThone <antoniothone6@gmail.com>
2024-03-24 04:27:08 +0100

    ...

---
+++ GestaoDeCondominios/Funcionario.java
@@ -0,0 +1,22 @@
+package AVC3.GestaoDeCondominios;
+
+public class Funcionario {
+    public class Funcionário {
+        private String nome;
+        private String cargo;
+    
+        public Funcionário(String nome, String cargo) {
+            this.nome = nome;
+            this.cargo = cargo;
+        }
+    
+        public String getNome() {
+            return nome;
+        }
+    
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+    }
+    
+}
