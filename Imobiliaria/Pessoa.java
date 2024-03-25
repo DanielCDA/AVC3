c78640142934f375c93d4af092a7f0f1208dee52
AntonioThone <antoniothone6@gmail.com>
2024-03-25 02:58:51 +0100

    Pessoa atributs

---
+++ Imobiliaria/Pessoa.java
@@ -0,0 +1,9 @@
+package Imobiliaria;
+
+public class Pessoa {
+    protected String nomeCompleto;
+    protected int anoNascimnto;
+    protected String genero;
+    protected int contacto;
+    protected String email;
+}
