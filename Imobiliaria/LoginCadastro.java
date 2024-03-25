633e5bef0abdc9bcbb30fc3f9abf58a9c7a4dc29
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:16:05 +0100

    getters e setters

--- Imobiliaria/LoginCadastro.java
+++ Imobiliaria/LoginCadastro.java
@@ -10,4 +10,28 @@ public class LoginCadastro {
         this.email = email;
         this.senha = senha;
     }
+
+     // Getters e Setters
+    // ...
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+
+    public void setEmail (String email){
+        this.email  = email;
+    }
+    public String getEmail (){
+        return email;
+    }
+
+    public void setSenha (String senha){
+        this.senha  = senha;
+    }
+    public String getSenha (){
+        return senha;
+    }
 }
