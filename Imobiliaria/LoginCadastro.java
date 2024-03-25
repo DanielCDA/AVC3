0436ea459b65eec40068a3604ef6c96655457375
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:15:38 +0100

    constructor

--- Imobiliaria/LoginCadastro.java
+++ Imobiliaria/LoginCadastro.java
@@ -4,4 +4,10 @@ public class LoginCadastro {
     protected String nome;
     protected String email;
     protected String senha;
+
+    public LoginCadastro(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
 }
