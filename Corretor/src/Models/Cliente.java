55f3c4332040dd6de51a24d6ab255fb7fb23ff74
AntonioThone <antoniothone6@gmail.com>
2024-03-25 04:40:00 +0100

    fixing

--- Corretor/src/Models/Cliente.java
+++ Corretor/src/Models/Cliente.java
@@ -5,6 +5,7 @@ package Corretor.src.Models;
 //import Imobiliaria.Pessoa;
 
 public class Cliente extends Pessoa {
-    public Cliente(String nome, String dataNascimento, String sexo, String morada,String email, int contacto, String password, String bi, Firma firma)
-    {super(nome, dataNascimento, sexo, morada, email, contacto, password, bi, firma);
+    public Cliente(String nome, String dataNascimento, String sexo, String morada,String email, int contacto, String password, String bi, Firma firma){
+        super(nome, dataNascimento, sexo, morada, email, contacto, password, bi, firma);
+    }
 }
