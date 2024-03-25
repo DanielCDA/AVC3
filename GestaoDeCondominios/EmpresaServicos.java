b428d739a7b997dc3d7453414889312ed57b2f70
AntonioThone <antoniothone6@gmail.com>
2024-03-25 01:29:25 +0100

    empresaServicos

---
+++ GestaoDeCondominios/EmpresaServicos.java
@@ -0,0 +1,15 @@
+puclic class EmpresaServicos{
+    private String nome;
+    private String tipoServico;
+
+    public EmpresaServicos(String nome, String tipoServico){
+        this.nome = nome;
+        this.tipoServico = tipoServico;
+    }
+
+    public void getNome(){
+        return nome;
+    }
+
+    
+}
