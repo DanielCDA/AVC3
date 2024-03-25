56a79489817ffaec2193ba8a73c591a2b2489538
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:19:22 +0100

    getters e setters

--- Imobiliaria/Terreno.java
+++ Imobiliaria/Terreno.java
@@ -8,4 +8,18 @@ public class Terreno {
         this.area =  area;
         this.localidade =  localidade;
     }
+
+    public void setArea(String area){
+        this.area = area;
+    }
+    public String getArea(){
+        return area;
+    }
+
+    public void setLocalidade(String localidade){
+        this.localidade = localidade;
+    }
+    public String localidade ( String zona1, String zona2){
+        return localidade;
+    }
 }
