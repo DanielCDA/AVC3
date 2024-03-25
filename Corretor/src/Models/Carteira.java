d40f24d5f039494124352b887f0a02d4ff5a29c4
AntonioThone <antoniothone6@gmail.com>
2024-03-25 04:15:51 +0100

    corretor + carteira

--- Corretor/src/Models/Carteira.java
+++ Corretor/src/Models/Carteira.java
@@ -1,5 +1,23 @@
 package Corretor.src.Models;
 
 public class Carteira extends BaseModel {
-    
+    private Corretor corretorResponsavel;
+    private Cliente clienteAssociated;
+
+    public Carteira(Corretor corretorResponsavel, Cliente clienteAssociated){
+        this.corretorResponsavel = corretorResponsavel;
+        this.clienteAssociated = clienteAssociated;
+    }
+    public void setCorretorResponsavel(Corretor corretorResponsavel){
+        this.corretorResponsavel = corretorResponsavel;
+    }
+    public Corretor getCorretorResponsavel(){
+        return corretorResponsavel;
+    }
+    public void setClienteAssociated(Cliente clienteAssociated){
+        this.clienteAssociated = clienteAssociated;
+    }
+    public Cliente getClienteAssociated(){
+        return clienteAssociated;
+    }
 }
