d01cd21d4c078c075b520f0f354f9254d76c93bb
AntonioThone <antoniothone6@gmail.com>
2024-03-25 06:08:27 +0100

    carteira controller

---
+++ Corretor/src/controller/CarteiraController.java
@@ -0,0 +1,18 @@
+package Corretor.src.controller;
+
+import java.util.ArrayList;
+
+import Corretor.src.Models.Carteira;
+import Corretor.src.Models.Corretor;
+import Corretor.src.Repositories.CarteiraRepository;
+
+public class CarteiraController {
+    private CarteiraRepository carteiraRepository;
+
+    public CarteiraController(CarteiraRepository repository) {
+        this.carteiraRepository = repository;
+    }
+
+    public ArrayList<Carteira> getCarteirasPorCorrector(Corretor corretor) {
+        return this.carteiraRepository.getCarteirasPorCorrector(corretor);
+}
