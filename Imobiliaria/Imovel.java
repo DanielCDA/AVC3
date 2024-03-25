771228c084f514af5539db8eedc4f2a4e7f74241
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:02:59 +0100

    Imovel

---
+++ Imobiliaria/Imovel.java
@@ -0,0 +1,8 @@
+package Imobiliaria;
+
+public class Imovel {
+    protected String  precoDeVenda;
+    protected String localizacao;
+    protected String endereco;
+    protected String anoDeConstrucao;
+}
