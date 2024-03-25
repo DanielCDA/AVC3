c7738faa75b84e3e711b0f0a1d49bc5e3a7cba70
AntonioThone <antoniothone6@gmail.com>
2024-03-25 04:19:33 +0100

    Cliente

---
+++ Corretor/src/Models/Cliente.java
@@ -0,0 +1,10 @@
+package Corretor.src.Models;
+
+//import Imobiliaria.Pessoa;
+
+//import Imobiliaria.Pessoa;
+
+public class Cliente extends Pessoa {
+    public Cliente(String nome, String dataNascimento, String sexo, String morada,String email, int contacto, String password, String bi, Firma firma)
+    {super(nome, dataNascimento, sexo, morada, email, contacto, password, bi, firma);
+}
