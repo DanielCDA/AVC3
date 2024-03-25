89a979e4d2e487ad51ee3404d357dc711e152fbf
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:18:58 +0100

    constructor

--- Imobiliaria/Terreno.java
+++ Imobiliaria/Terreno.java
@@ -3,4 +3,9 @@ package Imobiliaria;
 public class Terreno {
     private String area;
     private String localidade;
+
+    public Terreno (String area, String localidade){
+        this.area =  area;
+        this.localidade =  localidade;
+    }
 }
