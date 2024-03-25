11644fbf5a671f6542071c769f7fa0de2fefe695
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:09:56 +0100

    constructor

--- Imobiliaria/Vivendas.java
+++ Imobiliaria/Vivendas.java
@@ -5,4 +5,13 @@ public class Vivendas extends Imovel {
     private String anoDeConstrucao;
     private String area;
     private String tipologia;
+
+    public Vivendas(String anoDeConstrucao, String area,String precoDeVenda, String tipologia, String localizacao){
+        this.anoDeConstrucao = anoDeConstrucao;
+        this.area =  area;
+        this.tipologia = tipologia;
+        super.precoDeVenda = precoDeVenda;
+        super.localizacao = localizacao;
+
+    }
 }
