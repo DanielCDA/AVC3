39c300dc2ad03f7ce4aad3ed474bf20e79eb4e8d
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:12:28 +0100

    constructor

--- Imobiliaria/Apartamento.java
+++ Imobiliaria/Apartamento.java
@@ -3,4 +3,12 @@ package Imobiliaria;
 public class Apartamento extends Imovel {
     private String tipologia;
     private String area;
+
+    public Apartamento(String anoDeConstrucao, String area, String tipologia, String precoDeVenda, String localizacao){
+        super.localizacao = localizacao;
+        super.anoDeConstrucao = anoDeConstrucao;
+        this.area =  area;
+        this.tipologia = tipologia;
+        super.precoDeVenda = precoDeVenda;
+    }
 }
