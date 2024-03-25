7241a7c874baa954729eccb6e5ddd3e435f6ea5a
DanielCDA <danielagostinho15@gmail.com>
2024-03-25 14:47:20 +0100

    jogo

---
+++ CampeonatoEuropeuDeFutebol/Jogo.java
@@ -0,0 +1,43 @@
+package CampeonatoEuropeuDeFutebol;
+
+public class Jogo {
+    private Equipe equipeCasa;
+    private Equipe equipeVisitante;
+    private int[] resultado; // resultado do jogo (gols)
+
+    public Jogo(Equipe equipeCasa, Equipe equipeVisitante) {
+        this.equipeCasa = equipeCasa;
+        this.equipeVisitante = equipeVisitante;
+        this.resultado = new int[2]; // inicializar com 0 gols
+    }
+
+    public Equipe getEquipeCasa() {
+        return equipeCasa;
+    }
+
+    public void setEquipeCasa(Equipe equipeCasa) {
+        this.equipeCasa = equipeCasa;
+    }
+
+    public Equipe getEquipeVisitante() {
+        return equipeVisitante;
+    }
+
+    public void setEquipeVisitante(Equipe equipeVisitante) {
+        this.equipeVisitante = equipeVisitante;
+    }
+
+    public int[] getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(int[] resultado) {
+        this.resultado = resultado;
+    }
+
+    public void registrarResultado(int golsEquipeCasa, int golsEquipeVisitante) {
+        resultado[0] = golsEquipeCasa;
+        resultado[1] = golsEquipeVisitante;
+        // atualizar outras estatísticas do jogo
+    }
+}
