e39888708dae86c55065a3f22efae5b2af0fc53d
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:29:34 +0100

    Principal

---
+++ Imobiliaria/Principal.java
@@ -0,0 +1,8 @@
+package Imobiliaria;
+
+public class Principal {
+    private static final int TAMANHO_MAXIMO = 100;
+    private static LoginCadastro[] usuarios = new LoginCadastro[TAMANHO_MAXIMO];
+    private static int quantidadeUsuarios = 0;
+
+}
