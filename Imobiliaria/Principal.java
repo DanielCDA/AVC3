181e37b6a0a9dccb2269669fbcc3e37075bed4e2
AntonioThone <antoniothone6@gmail.com>
2024-03-25 03:30:30 +0100

    adding main function

--- Imobiliaria/Principal.java
+++ Imobiliaria/Principal.java
@@ -1,8 +1,13 @@
 package Imobiliaria;
 
+import java.util.Scanner;
+
 public class Principal {
     private static final int TAMANHO_MAXIMO = 100;
     private static LoginCadastro[] usuarios = new LoginCadastro[TAMANHO_MAXIMO];
     private static int quantidadeUsuarios = 0;
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean executando = true;
 }
