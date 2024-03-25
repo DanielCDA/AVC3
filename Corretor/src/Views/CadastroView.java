26e5dc9f90b39fb9dedb5793dfd1e4de86298b5a
AntonioThone <antoniothone6@gmail.com>
2024-03-25 04:53:37 +0100

    cadastro

---
+++ Corretor/src/Views/CadastroView.java
@@ -0,0 +1,22 @@
+package Views;
+
+public class CadastroView extends BaseView implements BaseViewInterface {
+
+    private FirmaView firma;
+
+   public CadastroView(FirmaView firma){
+       this.firma = firma;
+
+   }
+
+
+    public void requestCadastro(){
+        this.firma.show();
+    }
+
+
+    @Override
+    public Object show() {
+        return null;
+    }
+}
