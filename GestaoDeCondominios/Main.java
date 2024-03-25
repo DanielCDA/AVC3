5cb92c1ab0932775c46da9f04110d9ca5a933a15
AntonioThone <antoniothone6@gmail.com>
2024-03-25 02:31:00 +0100

    novos condominos...

--- GestaoDeCondominios/Main.java
+++ GestaoDeCondominios/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Condominio condominio = new Condominio();
 
+        Condomino condomino1 = new Condomino ("Antonio", "Predio A");
+        Condomino condomino2 = new Condomino("Maria", "Edificio B");
+        condominio.adicionarCondomino(condomino1);
+        condominio.adicionarCondomino(condomino2);
     }
 }
